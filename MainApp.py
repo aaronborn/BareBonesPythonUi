c08a9134dbdbc5558aa206698105f0863abb5e83
Aaron <63196435+aaronborn@users.noreply.github.com>
2024-09-13 16:32:23 -0400

    Adding a bare bones ui for python

---
+++ MainApp.py
@@ -0,0 +1,19 @@
+import tkinter as tk
+from tkinter import ttk, Menu
+
+
+class MainApp(tk.Tk):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        
+def run_app():
+    try:
+        # Load the stock tickers from the config file
+        #stock_list = read_stock_config("stocks.txt")
+        app = MainApp()
+        app.mainloop()
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+if __name__ == "__main__":
+    run_app()
